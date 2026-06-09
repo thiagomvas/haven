54d5cc6513a2cb5d95bad802045d07668f95ba9f
Thiago Vasconcelos <thiagomvas@gmail.com>
2026-06-09 13:25:25 -0300

    ci: Add prettier linting

--- src/Presentation/Haven.Web/eslint.config.js
+++ src/Presentation/Haven.Web/eslint.config.js
@@ -1,16 +1,23 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactHooks from "eslint-plugin-react-hooks";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
   {
     plugins: {
       "react-hooks": reactHooks,
+      prettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prettier/prettier": "warn",
     },
   }
 );
