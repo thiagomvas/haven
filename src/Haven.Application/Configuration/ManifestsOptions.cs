83fb13d281dee4dab971d6a52f68b53e9b0dbbb8
Thiago Vasconcelos <thiagomvas@gmail.com>
2026-07-09 13:57:08 -0300

    chore: Update default paths for manifests and volumes

--- src/Haven.Application/Configuration/ManifestsOptions.cs
+++ src/Haven.Application/Configuration/ManifestsOptions.cs
@@ -3,7 +3,7 @@ namespace Haven.Application.Configuration;
 public class ManifestsOptions
 {
     public const string SectionName = "Manifests";
-    public string ManifestsPath { get; set; } = "manifests";
+    public string ManifestsPath { get; set; } = "/data/manifests";
     public bool IncludeEnvValuesOnExample { get; set; } = true;
     public bool AutoSyncEnabled { get; set; } = true;
     public int SyncIntervalSeconds { get; set; } = 60;
--- src/Haven.Application/Configuration/VolumesOptions.cs
+++ src/Haven.Application/Configuration/VolumesOptions.cs
@@ -9,5 +9,5 @@ public class VolumesOptions
     /// occupies <c>{RootPath}/{serviceId}/{volumeId}</c>, which is bind-mounted into the
     /// container at deploy time. Must be a path the Docker daemon can bind-mount.
     /// </summary>
-    public string RootPath { get; set; } = "volumes";
+    public string RootPath { get; set; } = "/data/volumes";
 }
