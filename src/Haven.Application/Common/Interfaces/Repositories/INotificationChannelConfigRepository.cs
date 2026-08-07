45feb005bb5e62855cb13d040ff5d7e1355c333b
Thiago Vasconcelos <thiagomvas@gmail.com>
2026-08-07 15:17:23 -0300

    feat: add is_system_default column and GetSystemDefaultAsync lookup

--- src/Haven.Application/Common/Interfaces/Repositories/INotificationChannelConfigRepository.cs
+++ src/Haven.Application/Common/Interfaces/Repositories/INotificationChannelConfigRepository.cs
@@ -1,4 +1,5 @@
 using Haven.Application.Common.Messaging;
+using Haven.Domain;
 using Haven.Domain.Entities;
 
 namespace Haven.Application.Common.Interfaces.Repositories;
@@ -8,6 +9,7 @@ public interface INotificationChannelConfigRepository
     Task<Guid> AddAsync(NotificationChannelConfig config, CancellationToken cancellationToken);
     Task<NotificationChannelConfig?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
     Task<PagedResult<NotificationChannelConfig>> GetPagedAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
+    Task<NotificationChannelConfig?> GetSystemDefaultAsync(NotificationChannel channel, CancellationToken cancellationToken);
     Task UpdateAsync(NotificationChannelConfig config, CancellationToken cancellationToken);
     void Remove(NotificationChannelConfig config);
 }
--- src/Haven.Infrastructure/Persistence/Repositories/NotificationChannelConfigRepository.cs
+++ src/Haven.Infrastructure/Persistence/Repositories/NotificationChannelConfigRepository.cs
@@ -1,5 +1,6 @@
 using Haven.Application.Common.Interfaces.Repositories;
 using Haven.Application.Common.Messaging;
+using Haven.Domain;
 using Haven.Domain.Entities;
 using Haven.Infrastructure.Persistence.Extensions;
 
@@ -9,6 +10,10 @@ namespace Haven.Infrastructure.Persistence.Repositories;
 
 public class NotificationChannelConfigRepository(HavenDbContext context) : INotificationChannelConfigRepository
 {
+    public Task<NotificationChannelConfig?> GetSystemDefaultAsync(NotificationChannel channel, CancellationToken cancellationToken)
+        => context.NotificationChannelConfigs
+            .FirstOrDefaultAsync(c => c.Channel == channel && c.IsSystemDefault, cancellationToken);
+
     public Task<Guid> AddAsync(NotificationChannelConfig config, CancellationToken cancellationToken)
     {
         context.NotificationChannelConfigs.Add(config);
